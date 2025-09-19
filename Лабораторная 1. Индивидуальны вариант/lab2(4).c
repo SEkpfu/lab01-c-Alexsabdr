0eaf17993c73a5605b5c19ea946d2dfb3f4e00d3
Alexsandr <shurik1501@gmail.com>
2025-09-19 15:07:11 +0300

    Update lab2(4).c

--- Лабораторная 1. Индивидуальны вариант/lab2(4).c
+++ Лабораторная 1. Индивидуальны вариант/lab2(4).c
@@ -9,12 +9,13 @@ int main(void)
     x2 = x * x; 
     x3 = x2 * x;
     x5 = x3 * x2;
-    x4 = x2 * x2;
-    x14 = x5 * x5 * x4;
+    x7 = x5 * x2;
+    x14 = x7 * x7;
     y = x14 - x5 + x3 - 2;
     printf("%f", y); 
     return 0;
 
 }
+
 
     
